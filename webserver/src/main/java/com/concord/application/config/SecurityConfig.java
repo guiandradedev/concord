13fe701cb9395fd25db722bcb9c58b1a5f0b605f
mauriciobordon <mauriciolbordon@gmail.com>
2026-09-09 18:24:02 -0300

    fix(fix): just a fix. Keep going straight ahead, sir

--- webserver/src/main/java/com/concord/application/config/SecurityConfig.java
+++ webserver/src/main/java/com/concord/application/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 
 import lombok.RequiredArgsConstructor;
 
